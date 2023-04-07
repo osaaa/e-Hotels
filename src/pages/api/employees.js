3ae9e2df4cf512b99ea5de1c5f13dd9ba0848502
Cattuong Le <lcattuong@gmail.com>
2023-04-07 14:38:41 -0400

    Hotels, capacity, area, employee js files

---
+++ src/pages/api/employees.js
@@ -0,0 +1,32 @@
+/* Employee*/
+import mysql from 'mysql';
+
+export default async function handler(req, res) {
+
+  const client = mysql.createConnection({
+    host: "localhost",
+    user: "ehotel",
+    port: 3306,
+    password: "2132ehotel!",
+    database: "Hotels",
+  });
+
+  try {
+    client.connect();
+    const result = await new Promise((resolve, reject) => {
+      client.query('SELECT Employee_ID FROM Employee', (error, results) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  } finally {
+    client.end();
+  }
+}
